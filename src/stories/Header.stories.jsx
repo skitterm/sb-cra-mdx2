56e89e40e0ec410d655333ecea468ce4a71a0f85
Steven Kitterman <steven.kitterman@gmail.com>
2022-05-03 15:54:24 -0700

    Example

--- src/stories/Header.stories.jsx
+++ src/stories/Header.stories.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 
 import { Header } from './Header';
 
-export default {
-  title: 'Example/Header',
-  component: Header,
-  parameters: {
-    // More on Story layout: https://storybook.js.org/docs/react/configure/story-layout
-    layout: 'fullscreen',
-  },
-};
+// export default {
+//   title: 'Example/Header',
+//   component: Header,
+//   parameters: {
+//     // More on Story layout: https://storybook.js.org/docs/react/configure/story-layout
+//     layout: 'fullscreen',
+//   },
+// };
 
 const Template = (args) => <Header {...args} />;
 
